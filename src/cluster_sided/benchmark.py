d52273c69ec330cfe2666267dc99d922ff3267ba
Yannick Funk <yannickfunk@yahoo.de>
2021-03-01 10:05:00 +0100

    Move benchmark.py to cluster sided

--- src/cluster_sided/benchmark.py
+++ src/cluster_sided/benchmark.py
@@ -39,11 +39,11 @@ class Benchmark:
         return performance_matrix.replace([np.inf], 1.0e+100).replace([np.nan, 0.0], 1.0e-16)
 
     @staticmethod
-    def _normalized_residual_matrix(residual_matrix):
-        for index in residual_matrix.index:
-            min_res = residual_matrix.loc[index].min()
-            residual_matrix.loc[index] = residual_matrix.loc[index].map(lambda x: x / min_res)
-        return residual_matrix
+    def _normalized_residual_matrix(matrix):
+        for index in matrix.residuals.index:
+            min_res = matrix.residuals.loc[index].min()
+            matrix.residuals.loc[index] = matrix.residuals.loc[index].map(lambda x: x / min_res)
+        return matrix
 
     @staticmethod
     def _relative_residuals(solver_name, solver):
@@ -51,9 +51,9 @@ class Benchmark:
         return residuals
 
     @staticmethod
-    def _res_sums(residual_matrix):
-        sums = np.array([residual_matrix[e].sum() for e in residual_matrix.columns])
-        return pd.DataFrame([sums], columns=residual_matrix.columns)
+    def _res_sums(matrix):
+        sums = np.array([matrix.residuals[e].sum() for e in matrix.residuals.columns])
+        return Matrix(matrix.path, pd.DataFrame([sums], columns=matrix.residuals.columns))
 
     @staticmethod
     def limit_residual_column(column):
@@ -70,12 +70,12 @@ class Benchmark:
 
     def get_label_df(self):
         residual_matrices = self.get_matrices_with_normalized_residuals()
-        non_empty_indices = [i for i, e in enumerate(residual_matrices) if not e.empty]
-        matrices_res_sums = [self._res_sums(e)
-                             for e in residual_matrices if not e.empty]
-        filepaths = np.array([e["filename"] for e in self.benchmark_results])[non_empty_indices]
-        matrices_res_sums_df = pd.concat(matrices_res_sums) / len(self.measurement_points)
-        matrices_res_sums_df.insert(0, "path", filepaths)
+        matrices = [self._res_sums(e)
+                             for e in residual_matrices if not e.residuals.empty]
+        res_sums = [e.residuals for e in matrices]
+        paths = [e.path for e in matrices]
+        matrices_res_sums_df = pd.concat(res_sums) / len(self.measurement_points)
+        matrices_res_sums_df.insert(0, "path", paths)
         return matrices_res_sums_df
 
     def get_matrices(self):
@@ -87,16 +87,16 @@ class Benchmark:
         return residual_matrices
 
     def get_matrices_with_normalized_residuals(self):
-        return [self._normalized_residual_matrix(e.residuals) for e in self.get_matrices()]
+        return [self._normalized_residual_matrix(e) for e in self.get_matrices()]
 
     def plot_solver_histogram(self):
-        matrices_res_sums = [self._res_sums(e)
+        res_sums = [self._res_sums(e).residuals
                              for e in self.get_matrices_with_normalized_residuals()]
-        matrices_res_sums_df = pd.concat(matrices_res_sums) / len(self.measurement_points)
+        res_sums_df = pd.concat(res_sums) / len(self.measurement_points)
         bins = np.linspace(1, 20, 50)
-        matrices_res_sums_df.plot.hist(bins=bins, alpha=0.5)
+        res_sums_df.plot.hist(bins=bins, alpha=0.5)
         plt.show()
-        for solver in matrices_res_sums_df.columns:
-            ax = matrices_res_sums_df[solver].plot.hist(bins=bins, alpha=0.5, legend=True)
+        for solver in res_sums_df.columns:
+            ax = res_sums_df[solver].plot.hist(bins=bins, alpha=0.5, legend=True)
             plt.show()
 
