403305db4edd7fddaa928e3be92762ad4cf8e0cf
Yannick Funk <yannickfunk@yahoo.de>
2021-03-27 13:37:54 +0100

    Add evaluations for fc net

--- src/cluster_sided/nn_training/eval.py
+++ src/cluster_sided/nn_training/eval.py
@@ -5,6 +5,7 @@ from torch import nn
 from ignite.engine import create_supervised_evaluator
 from ignite.metrics import Accuracy, Loss
 import numpy as np
+from sklearn.metrics import classification_report, confusion_matrix
 
 
 def get_top_n_accuracy(n):
@@ -51,6 +52,69 @@ def get_slowdown():
         return slowdowns
 
 
+def get_classification_report():
+    for features, labels in val_loader:
+        features = features.to(device)
+        predictions = model(features).cpu().detach().numpy().argmax(axis=1)
+        labels = labels.numpy()[:, 0]
+        return classification_report(labels, predictions, target_names=SOLVERS)
+
+
+def get_confusion_matrix():
+    for features, labels in val_loader:
+        features = features.to(device)
+        predictions = model(features).cpu().detach().numpy().argmax(axis=1)
+        labels = labels.numpy()[:, 0]
+        return confusion_matrix(labels, predictions)
+
+
+def save_confusion_matrix(cm,
+                          target_names,
+                          title='Confusion matrix',
+                          cmap=None,
+                          normalize=True):
+    import matplotlib.pyplot as plt
+    import itertools
+
+    accuracy = np.trace(cm) / float(np.sum(cm))
+    misclass = 1 - accuracy
+
+    if cmap is None:
+        cmap = plt.get_cmap('Blues')
+
+    plt.figure(figsize=(8, 6))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+
+    if target_names is not None:
+        tick_marks = np.arange(len(target_names))
+        plt.xticks(tick_marks, target_names, rotation=45)
+        plt.yticks(tick_marks, target_names)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+
+    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        if normalize:
+            plt.text(j, i, "{:0.4f}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+        else:
+            plt.text(j, i, "{:,}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+
+
+    plt.tight_layout()
+    plt.ylabel('True solver')
+    plt.xlabel('Predicted solver')
+    plt.savefig("cm.png")
+
+
+SOLVERS = ["bicg", "bicgstab", "cg", "cgs", "fcg", "gmres", "idr"]
 df = pd.read_csv("val_set.csv")
 eval_set = MatrixEvalDataset(df)
 val_loader = torch.utils.data.DataLoader(eval_set, batch_size=len(eval_set),
@@ -92,3 +156,5 @@ for i in range(1, 8):
 
 print("Mean slowdown: ", np.mean(get_slowdown()))
 
+print(get_classification_report())
+save_confusion_matrix(get_confusion_matrix(), SOLVERS, normalize=False)
